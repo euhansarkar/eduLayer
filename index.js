ac309de97bd914e0f7e999ab6740f0324b981235
euhansarkar <euhansarkar@gmail.com>
2022-10-24 15:27:26 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require(`express`);
+const app = express();
+const port = process.env.PORT || 5000;
+const courses = require(`./data/courses.json`)
+const cors = require(`cors`);
+
+app.use(cors());
+
+app.get(`/`, (req, res) => {
+    res.send(`mumbaui is coming`)
+})
+
+app.get(`/courses`, (req, res) => {
+    res.send(courses)
+})
+
+app.listen(port, () => {
+    console.log(`nodemon running on port ${port}`)
+})
